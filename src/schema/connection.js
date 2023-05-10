5205e3a6b2ec37f4b5efb87283499c5f83545f70
Pxrsona <obando_armando28@hotmail.com>
2023-05-09 19:44:44 -0600

    Rename somethings <3

--- src/schema/connection.js
+++ src/schema/connection.js
@@ -7,7 +7,7 @@ import {
   DB_NAME,
 } from "../configuration/configDB.js";
 
-export const pool = createPool({
+export const database = createPool({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
