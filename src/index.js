db885417896aa11dc2eb9f5fcda48721a06f1dbe
Pxrsona <obando_armando28@hotmail.com>
2023-05-12 11:59:23 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,17 +2,7 @@ import app from "./app.js";
 import { PORT } from "./configuration/configDB.js";
 import fs from "fs";
 import https from "https";
-import cors from "cors";
 
 
-/*const options = {
-    key: fs.readFileSync('');
-    cert: fs.readFileSync('');
-};*/
-
-//https.createServer(options, app).listen(PORT,() => console.log("Server listen in port " + PORT));
 console.log("Server listen in port " + PORT);
-/*app.use(cors({
-    //dominios disponibles
-}));*/
 app.listen(PORT);
