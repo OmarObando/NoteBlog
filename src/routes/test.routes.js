bfb5bfa5404b1e111177b40097767f5d38b837cf
Pxrsona <obando_armando28@hotmail.com>
2023-05-12 10:44:51 -0600

    test twilio

--- src/routes/test.routes.js
+++ src/routes/test.routes.js
@@ -1,5 +1,11 @@
 import { Router } from "express";
 import { ping } from "../controllers/test.controllers.js";
+import {sendSMS, sendWS} from "../messages/twilio.js";
+import{
+  TWILIO_ACCOUNT_SID,
+  TWILIO_AUTH_TOKEN,
+  TWILIO_PHONE_NUMBER
+} from "..//messages//twilio.config.js";
 
 const router = Router();
 
@@ -7,6 +13,12 @@ router.get("/ping", function (req, res) {
   ping
     .then((row) => res.status(200).send(row[0]))
     .catch((err) => res.status(500).send(err));
+});
+
+
+router.post("/sms", function(req, res){
+    sendWS();
+    //sendSMS("+522281300932","this is the test").then().catch();
 });
 
 export default router;
