0e4feb981d9b3e0c78114492f08820b3796074d7
Pxrsona <obando_armando28@hotmail.com>
2023-05-12 10:46:58 -0600

    test updated xoxoxoxox

--- src/controllers/test.controllers.js
+++ src/controllers/test.controllers.js
@@ -1,3 +1,3 @@
-import { pool } from "../schema/connection.js";
+import { database } from "../schema/connection.js";
 
-export const ping = Promise.resolve(pool.query("SELECT 1 + 1 AS result"));
+export const ping = Promise.resolve(database.query("SELECT 1 + 1 AS result"));
