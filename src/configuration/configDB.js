1ba7d6c131214a632f1769f24c85a99566a103fe
Pxrsona <obando_armando28@hotmail.com>
2023-05-12 10:46:06 -0600

    shomething something

--- src/configuration/configDB.js
+++ src/configuration/configDB.js
@@ -8,3 +8,4 @@ export const DB_HOST = "localhost";//process.env.DB_HOST;
 export const DB_USER = "root";//process.env.DB_USER;
 export const DB_PASSWORD = "aoom08";//= process.env.DB_PASSWORD;
 export const DB_NAME = "noteblog";//process.env.DB_NAME;
+export const TOKEN_KEY = "1234"
